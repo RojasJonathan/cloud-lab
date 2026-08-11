49a8c2f2ce9045009de8f4410261d73e863320b8
RojasJonathan <jonjrojas@gmail.com>
2026-08-11 17:45:44 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,37 @@ resource "aws_s3_bucket" "evidence" {
   bucket_prefix = "cloud-grc-evidence-"
 
   tags = {
-    Project     = "Cloud lab"
+    Project     = "Cloud GRC Lab"
     Environment = "Lab"
+  }
+}
+
+# Control 1: Block public access
+resource "aws_s3_bucket_public_access_block" "grc_lab" {
+  bucket = aws_s3_bucket.grc_lab.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# Control 2: Enable versioning
+resource "aws_s3_bucket_versioning" "grc_lab" {
+  bucket = aws_s3_bucket.grc_lab.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Control 3: Encryption at rest
+resource "aws_s3_bucket_server_side_encryption_configuration" "grc_lab" {
+  bucket = aws_s3_bucket.grc_lab.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
   }
 }
